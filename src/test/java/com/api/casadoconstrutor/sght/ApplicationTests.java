4cb8bb95a8564420ac2a851b302e062a608578a5
João Paulo <costajoaopaulo113@gmail.com>
2024-12-17 09:12:08 -0300

    add: upload back-end

---
+++ src/test/java/com/api/casadoconstrutor/sght/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.casadoconstrutor.sght;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
